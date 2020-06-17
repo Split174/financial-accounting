947dfa54ab64d6e68d4fb9cfabe5d3b1c5444072
Sergey Popov <sergei.popov174@gmail.com>
2020-06-18 02:10:23 +0500

    fix DB filename

--- src/config.py
+++ src/config.py
@@ -5,5 +5,5 @@ class Config:
     """
     Сonfig class for app_flask
     """
-    DB_CONNECTION = os.getenv('DB_CONNECTION', 'db.sqlite')
+    DB_CONNECTION = os.getenv('DB_CONNECTION', 'data_base.db')
     SECRET_KEY = os.getenv('SECRET_KEY')
