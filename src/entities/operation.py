0218daf3c0c6e642395171575c53ae130fe72e3a
Seva Nazarov <mr.seva99@mail.ru>
2020-06-24 20:23:52 +0500

    fix operations database and models

--- src/entities/operation.py
+++ src/entities/operation.py
@@ -1,27 +1,40 @@
+"""
+module with data classes for data conversion
+"""
 from dataclasses import dataclass
-from datetime import date
+from datetime import datetime
 from typing import Optional
+from decimal import Decimal
 
 
 @dataclass
 class Operation:
+    """
+    dataclass for input data conversion
+    """
     type_operation: str
-    amount: int
+    amount: Decimal
     category_id: Optional[int]
-    datetime: Optional[date]
+    datetime: Optional[datetime]
     description: Optional[str]
 
 
 @dataclass
 class GetOperation(Operation):
+    """
+    dataclass for get data conversion
+    """
     id: int
     user_id: int
 
 
 @dataclass
 class UpdateOperation:
+    """
+    dataclass for input data conversion
+    """
     type_operation: Optional[str]
-    amount: Optional[int]
+    amount: Optional[Decimal]
     category_id: Optional[int]
-    datetime: Optional[date]
+    datetime: Optional[datetime]
     description: Optional[str]
